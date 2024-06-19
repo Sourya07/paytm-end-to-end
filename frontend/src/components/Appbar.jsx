224695f4315a87bca07c5f23a27f3651cc10cb24
Sourya07 <singhsourya137@gmail.com>
2024-06-20 04:42:24 +0530

    some basic changes

--- frontend/src/components/Appbar.jsx
+++ frontend/src/components/Appbar.jsx
@@ -6,7 +6,7 @@ export const Appbar = () => {
         </div>
         <div className="flex">
             <div className="flex flex-col justify-center h-full mr-4">
-                Hello
+                This is your account 
             </div>
             <div className="rounded-full h-12 w-12 bg-slate-200 flex justify-center mt-1 mr-2">
                 <div className="flex flex-col justify-center h-full text-xl">
